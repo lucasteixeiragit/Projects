c1ff61b8db13a7915a2abd952d5eb7eb7f65955c
lucasteixeiragit <lucas.teixeiracarv@gmail.com>
2026-03-20 21:47:45 -0300

    20260320

---
+++ Projects/In Progress/Quebra-cabecas/scripts/puzzle.js
@@ -0,0 +1,17 @@
+var tabuleiro = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
+
+var objetivo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
+
+function embaralhar() {
+    for (var i = 0; i <2 100; i++) // Embaralha o tabuleiro 100 vezes, tornando possivel resolver, por serem movimentos validos
+      {
+        var vazio = tabuleiro.indexOf(0); //identifica posicao do espaço vazio (0) dentro do array
+        var vizinhos = []; //cria um array para armazenar os vizinhos do espaço vazio
+
+        if (vazio >= 4)
+
+      }
+
+    
+
+    }
